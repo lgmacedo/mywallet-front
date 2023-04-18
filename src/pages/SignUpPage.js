342592738a729f23c4761679ea8ba5f14b9e864e
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-18 15:52:11 -0300

    SignUp page implemented

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,24 +1,100 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import axios from "axios";
+import { useState } from "react";
 
 export default function SignUpPage() {
+  const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+  });
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    confirmaSenha: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function signup(e) {
+    e.preventDefault();
+    if (form.senha !== form.confirmaSenha) {
+      alert("Senhas inseridas não conferem");
+      return;
+    }
+    const signup = {
+      nome: form.nome,
+      email: form.email,
+      senha: form.senha,
+    };
+    const promise = api.post("/users", signup);
+    promise.then(signUpSuccess);
+    promise.catch(signUpFailed);
+  }
+
+  function signUpSuccess() {
+    navigate("/");
+  }
+
+  function signUpFailed(err) {
+    console.log(err);
+    if(err.response.status === 409){
+      alert("Email já cadastrado.");
+    }else if(err.response.status === 422){
+      alert("Confira seus dados. Todos os campos são obrigatórios, o email deve ter formato válido e a senha deve ter no mínimo 3 caracteres.");
+    }else{
+      alert("Erro inesperado. Tente novamente.");
+    }
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signup}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input
+          required
+          name="nome"
+          value={form.nome}
+          onChange={(e) => handleChange(e)}
+          placeholder="Nome"
+          type="text"
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={(e) => handleChange(e)}
+          placeholder="E-mail"
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          required
+          name="senha"
+          value={form.senha}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          required
+          name="confirmaSenha"
+          value={form.confirmaSenha}
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link to="/">Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -27,4 +103,4 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
